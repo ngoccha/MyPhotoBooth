97fbb1c1f963797cd4301aaeb6b30fb044336cbb
Ngoc Ha <ngocha38450510@gmail.com>
2025-07-31 10:12:38 +0700

    Initial Commit

---
+++ MyPhotoBooth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPhotoBooth
+//
+//  Created by iKame Elite Fresher 2025 on 7/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
